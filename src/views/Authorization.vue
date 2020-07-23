47cea178d9b8efeb226f0f18d39b1420316352c1
Nikita <nikita.savchuk.2000@gmail.com>
2020-07-23 17:38:03 +1000

    create and show my order

--- src/views/Authorization.vue
+++ src/views/Authorization.vue
@@ -60,8 +60,6 @@ export default {
     },
 
     checkForm(e) {
-      this.error = '';
-
       if (this.password.length < 6) {
         this.error = 'Пароль должен содержать больше 6 символов';
       }
