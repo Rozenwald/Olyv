db011cf8cf2f462730b71c9060566cdb6f54cd3c
SavchukNikita <nikita.savchuk.2000@gmail.com>
2020-09-18 19:53:12 +1000

    bottom sheet v2 pre

--- src/components/BottomNavigation.vue
+++ src/components/BottomNavigation.vue
@@ -44,7 +44,7 @@ export default {
     showLoginDialog() {
       this.$store.dispatch('showLoginDialog', true);
     },
-    /* clickBtn(index, routeName) {
+    clickBtn(index, routeName) {
       if ((index === 2 || index === 3) && !this.isAuth) {
         this.showLoginDialog();
       } else if (index === 2) {
@@ -52,9 +52,6 @@ export default {
       } else {
         this.route(routeName);
       }
-    }, */
-    clickBtn() {
-      this.$store.dispatch('setMainSheetStatus', 'half');
     },
   },
 };
