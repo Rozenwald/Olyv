fa2c549c751cb37eb19cabe5c7bd3f8318e2fc84
Lavr <rozenskys@gmail.com>
2020-12-01 20:08:25 +1000

    new

--- src/components/myOrders/AwaitOrders.vue
+++ src/components/myOrders/AwaitOrders.vue
@@ -56,7 +56,7 @@ export default {
             dialogMessages.getBody('notAuthentucate'),
             true,
             true,
-            this.$router.push('auth'),
+            () => { this.$router.push({ name: 'auth' }); },
           );
           break;
         case 'NotExist':
