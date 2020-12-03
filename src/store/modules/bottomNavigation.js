d0377b17efb6025c9bd39361b1a6e2cecb5d614e
rozenwald <rozenwald@inbox.ru>
2020-12-03 18:04:27 +1000

    'fix'

---
+++ src/store/modules/bottomNavigation.js
@@ -0,0 +1,35 @@
+const mutations = {
+  showBottomNavigation(state, flag) {
+    state.show = flag;
+  },
+};
+
+const actions = {
+  showBottomNavigation({ commit }, show) {
+    commit('showBottomNavigation', show);
+  },
+};
+
+const getters = {
+  getItemsBottomNavigaion: (state) => state.items,
+  isVisibleBottomNavigation: (state) => state.show,
+};
+
+const state = {
+  items: [
+    { title: 'Заказчик', icon: 'MyOrder', routeName: 'myAwaitOrders' },
+    { title: 'Исполнитель', icon: 'OrdersList', routeName: 'allOrder' },
+    { title: '', icon: 'Plus' },
+    { title: 'Сообщения', icon: 'Messages', routeName: 'allChat' },
+    { title: 'Профиль', icon: 'User', routeName: 'customerProfile' },
+  ],
+  show: true,
+};
+
+export default {
+  state,
+  getters,
+  mutations,
+  actions,
+
+};
