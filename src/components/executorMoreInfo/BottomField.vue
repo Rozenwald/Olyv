825d3e002c556165f26392f476cb65e4e0b46d4c
rozenwald <rozenwald@inbox.ru>
2020-12-10 21:49:47 +1000

    'fix-bottomfield-immortal-loading'

--- src/components/executorMoreInfo/BottomField.vue
+++ src/components/executorMoreInfo/BottomField.vue
@@ -106,11 +106,7 @@ export default {
         case 'success':
           if (this.orderType === ('all' || 'keyword')) {
             this.$store.dispatch('setType', 'await');
-<<<<<<< HEAD
           } else if (this.orderType === ('await' || 'process')) {
-=======
-          } else if (this.orderType === 'await') {
->>>>>>> 2f19a5b92680c4c5613768663746b66d0f04c07d
             this.$store.dispatch('setType', 'all');
           }
 
